c8d319ac1e3a22ce25ddf156dece631ad84e3a3a
Pat Maddox <pat.maddox@gmail.com>
2009-02-08 19:10:42 -0800

    rss generator

--- lib/daily_deal.rb
+++ lib/daily_deal.rb
@@ -2,10 +2,12 @@ require "rubygems"
 require "hpricot"
 require "open-uri"
 require "ostruct"
+require "guid"
+require "builder"
 
 module DailyDeal
-  class PageParser
-    def self.parse(contents)
+  class << self
+    def parse(contents)
       doc = Hpricot(contents) / ".bookdetails"
       result = OpenStruct.new
       result.title = (doc/"h3"/"a").first.innerText
@@ -13,5 +15,37 @@ module DailyDeal
       result.cover_image = "http://www.apress.com" + (doc/".cover"/"img").first[:src]
       result
     end
+
+    def rss(result)
+      xml = Builder::XmlMarkup.new :indent => 2
+      xml.instruct!
+      xml.rss :version => '2.0', "xmlns:atom" => "http://www.w3.org/2005/Atom" do
+        xml.channel do
+          xml.title "Apress Deal of the Day"
+          xml.description "Apress Deal of the Day"
+          deal_link = "http://www.apress.com/info/dailydeal"
+          xml.link deal_link
+          xml.tag! "atom:link", :rel => "self", :href => "http://www.patmaddox.com/apress_daily_deal"
+
+          xml.item do
+            xml.title result.title
+            xml.description result.description
+            xml.link deal_link
+            xml.pubDate DateTime.now.rfc822
+            xml.guid Guid.new.to_s, :isPermaLink => false
+          end
+        end
+      end
+    end
+
+    def fetch
+      rss parse(open("http://www.apress.com/info/dailydeal"))
+    end
+  end
+end
+
+class DateTime
+  def rfc822
+    strftime '%a, %d %b %Y %H:%M:%S %z'
   end
 end
